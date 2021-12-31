7eb133b84c3865a5e7a7cebc98235bb859758028
Piyush Tanti <magicianworld99@gmail.com>
2021-12-31 15:59:50 +0530

    Added Routes

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use('/product/', router);
+
 app.use(cors())
 app.use(express.json());
 app.use(express.static('uploads'));
 
-app.use('/product/', router);
 
 app.listen(port, () => {
     console.log(`App Start Listening at PORT ${port}`);
