6bef7f51e6a5744f4ece3484c843eda756da859c
Piyush Tanti <magicianworld99@gmail.com>
2021-12-31 19:40:00 +0530

    Commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use('/api/product', productRouter);
+app.use("/api/product", productRouter);
 
 app.use(cors())
 app.use(express.json());
