6c97999753ba1b5bf5dfab3e86e284a5ece852e5
Piyush Tanti <magicianworld99@gmail.com>
2022-01-03 13:02:00 +0530

    Commit

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use('/api/product', productRouter);
 app.use((req, res, next) => {
     res.json({message: "App is Running in Heroku"})
 })
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+  })
 
 app.use(cors())
 app.use(express.json());
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000;
 const server = http.createServer(app, (req, res) => {
     const headers = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, DELETE',
     };
 
     res.writeHead(200, headers);
