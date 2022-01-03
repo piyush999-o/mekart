3237e03526b6b369bc5f53da897ea5687e14711c
Piyush Tanti <magicianworld99@gmail.com>
2022-01-03 21:07:20 +0530

    Commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     res.header({"Access-Control-Allow-Origin": "*"});
     next();
-  }) 
+})
 
 app.use(cors())
 app.use(express.json());
